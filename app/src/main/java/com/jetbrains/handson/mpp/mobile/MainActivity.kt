426daf7c278ac8503182d5a61c90b5ca65934062
OliOB <Oli.OBrien@softwire.com>
2021-07-13 10:40:33 +0100

    Added UI elements and resource string for dropdowns

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -2,6 +2,8 @@ package com.jetbrains.handson.mpp.mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), ApplicationContract.View {
@@ -12,6 +14,22 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
         val presenter = ApplicationPresenter()
         presenter.onViewTaken(this)
+
+        val departureStationDropdown: Spinner = findViewById(R.id.spinner)
+        val arrivalStationDropdown: Spinner = findViewById(R.id.spinner2)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.stations_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            departureStationDropdown.adapter = adapter
+            arrivalStationDropdown.adapter = adapter
+        }
+
     }
 
     override fun setLabel(text: String) {
