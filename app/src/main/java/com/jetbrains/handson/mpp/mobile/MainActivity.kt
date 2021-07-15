1039055d9d31cad9b591cdb40616aed0b75d9ef0
OliOB <Oli.OBrien@softwire.com>
2021-07-15 14:18:36 +0100

    Added searchable lists to the android function

--- app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/mobile/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.jetbrains.handson.mpp.mobile
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
+import androidx.activity.result.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,15 +17,32 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
     lateinit var departureStationDropdown: Spinner
     lateinit var arrivalStationDropdown: Spinner
+    lateinit var departureStationText: EditText
+    lateinit var arrivalStationText: EditText
+
+    val departureStationStart = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            departureStationText.setText(result.data!!.getStringExtra("Result"))
+        }
+    }
+
+    val arrivalStationStart = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            arrivalStationText.setText(result.data!!.getStringExtra("Result"))
+        }
+    }
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val presenter = ApplicationPresenter()
         presenter.onViewTaken(this)
-
+        /*
         departureStationDropdown = findViewById(R.id.spinner)
         arrivalStationDropdown = findViewById(R.id.spinner2)
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -37,6 +56,14 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
             // Apply the adapter to the spinner
             departureStationDropdown.adapter = adapter
             arrivalStationDropdown.adapter = adapter
+        }*/
+        departureStationText = findViewById(R.id.departureStationText)
+        arrivalStationText = findViewById(R.id.arrivalStationText)
+        departureStationText.setOnClickListener{
+            departureStationStart.launch(Intent(this,SearchStations::class.java))
+        }
+        arrivalStationText.setOnClickListener{
+            arrivalStationStart.launch(Intent(this,SearchStations::class.java))
         }
 
         val button: Button = findViewById(R.id.button)
@@ -51,8 +78,8 @@ class MainActivity : AppCompatActivity(), ApplicationContract.View {
 
     // TODO: to catch exception for when originDest same as targetDest, or app crashes.
     fun getData(view: ApplicationContract.View) {
-        val departureStation=departureStationDropdown.selectedItem.toString()
-        val arrivalStation = arrivalStationDropdown.selectedItem.toString()
+        val departureStation=departureStationText.text.toString()
+        val arrivalStation = arrivalStationText.text.toString()
         val url = getAPIURLWithSelectedStations(arrivalStation,departureStation)
         val presenter = ApplicationPresenter()
         print(presenter.getData(view,url))
